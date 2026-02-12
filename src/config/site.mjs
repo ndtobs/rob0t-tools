48379f9d0848c36d86eeb7baa4fe6c049ff1f857
ndtobs <>
2026-02-12 16:52:26 +0000

    Astro Base template - clean and simple

---
+++ src/config/site.mjs
@@ -0,0 +1,9 @@
+export const SITE = {
+  title: "rob0t.tools",
+  description: "Network tooling for the GitOps era",
+  defaultLanguage: "en",
+  url: "https://rob0t.tools",
+  author: "ndtobs",
+  ogImage: "/og-image.png",
+  web3formsAccessKey: "",
+};
